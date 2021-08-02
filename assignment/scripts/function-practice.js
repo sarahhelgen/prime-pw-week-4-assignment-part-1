fb52d06d0842d995a3315e754265200be2abccf2
Sarah Helgen <sarah.helgen@gmail.com>
2021-08-02 10:23:06 -0500

    question 2 complete

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,9 +15,12 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  console.log( 'in helloName', name );
+  let greeting = `Hello ${name}!`
+  return greeting;
 }
 // Remember to call the function to test
+console.log( helloName('Sarah'));
 
 
 // 3. Function to add two numbers together & return the result
